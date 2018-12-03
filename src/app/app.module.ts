ba6bfdb00b3646e34c3898112eb80955962befe8
angieVilla <kyutvillan@gmail.com>
2018-12-02 20:56:30 -0500

    Routes
    ejemplo de como realizar la tabla de routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EspecialistasComponent } from './especialistas/especialistas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EspecialistasComponent
   ],
   imports: [
     BrowserModule,
